3c59cd3ab123131920c6ea17af01faa1fad9ff49
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-14 12:27:31 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -15,13 +15,21 @@ url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV
 r = requests.get(url)
 open('DIYP-v4.txt', 'wb').write(r.content)
 
-keywords = ['凤凰卫视', '天映', '香港卫视', '东森', '龙祥', '公视', '人间', '大爱', '三立']  # 需要提取的关键字列表
+keywords = ['凤凰卫视', '凤凰资讯', 'TVB', '天映', '香港卫视']  # 需要提取的关键字列表
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
 with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('HK.txt', 'w', encoding='utf-8') as HK:
-    HK.write('\n港澳台频道,#genre#\n')
+    HK.write('\n港澳频道,#genre#\n')
     for line in file:
         if re.search(pattern, line):  # 如果行中有任意关键字
             HK.write(line)  # 将该行写入输出文件
+
+keywords = ['民视', '中视', '台视', '华视', '新闻台', '东森', '龙祥', '公视', '三立', '大爱', '人间']  # 需要提取的关键字列表
+pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
+with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('TW.txt', 'w', encoding='utf-8') as TW:
+    TW.write('\n台湾频道,#genre#\n')
+    for line in file:
+        if re.search(pattern, line):  # 如果行中有任意关键字
+            TW.write(line)  # 将该行写入输出文件
 
 # 读取要合并的香港频道和台湾频道文件
 file_contents = []
