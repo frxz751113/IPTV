9a903b69cf5e9865eb4afc088db0a7ac870ee61c
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-16 19:11:33 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -45,8 +45,8 @@ with open("GAT.txt", "w", encoding="utf-8") as output:
 
 # 扫源
 urls = [
-"https://fofa.info/result?qbase64=JTIyaXB0di9saXZlL3poX2NuLmpzJTIyJTIwJTI2JTI2JTIwY291bnRyeSUzRCUyMkNOJTIy",	        # 娄 底
-
+"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rmW5YyXIg%3D%3D",        # 湖 北
+# "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rmW5YyXIg%3D%3D",        # 湖 北    
 ]
 
 
