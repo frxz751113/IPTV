8912bc3e1b714b71c348c64c6e2834c6e9255c69
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-16 17:27:46 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -156,7 +156,7 @@ for url in urls:
     driver.quit()
 
     # 查找所有符合指定格式的网址
-    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:6666"  # 设置匹配的格式，如http://8.8.8.8:8888
+    pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:9901"  # 设置匹配的格式，如http://8.8.8.8:8888
     urls_all = re.findall(pattern, page_content)
     # urls = list(set(urls_all))  # 去重得到唯一的URL列表
     urls = set(urls_all)  # 去重得到唯一的URL列表
