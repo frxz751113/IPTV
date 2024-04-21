3a9a96c078225557c64c1e190c58ea2d2a287f71
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-21 09:27:14 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -429,11 +429,11 @@ with open("hn.txt", 'w', encoding='utf-8') as file:
                 channel_counters[channel_name] = 1
                         
     channel_counters = {}
-    file.write('影视频道,#genre#\n')
+    file.write('综合频道,#genre#\n')
     for result in results:
         channel_name, channel_url, speed = result
-        if '影' in channel_name or '剧' in channel_name or '经典' in channel_name or '4K' in channel_name or '石家庄娱乐' in channel_name:
-       # if 'CCTV' not in channel_name and '卫视' not in channel_name and '卡' not in channel_name and '儿' not in channel_name and '文' not in channel_name and '新' not in channel_name and '山东' not in channel_name and '河北' not in channel_name and '哈哈' not in channel_name and '临沂' not in channel_name and '公共' not in channel_name and 'CETV' not in channel_name and '交通' not in channel_name and '冬' not in channel_name and '梨园' not in channel_name and '民生' not in channel_name and '综合' not in channel_name and '法制' not in channel_name and '齐鲁' not in channel_name and '自办' not in channel_name and '都市' not in channel_name:
+        #if '影' in channel_name or '剧' in channel_name or '经典' in channel_name or '4K' in channel_name or '石家庄娱乐' in channel_name:
+        if 'CCTV' not in channel_name and '卫视' not in channel_name and '卡' not in channel_name:
             if channel_name in channel_counters:
                 if channel_counters[channel_name] >= result_counter:
                     continue
