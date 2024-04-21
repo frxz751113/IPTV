8b5cd620f947b6b3b1b5bd9ef292551b179835fc
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-21 11:30:34 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -80,7 +80,7 @@ def modify_urls(url):
 
 def is_url_accessible(url):
     try:
-        response = requests.get(url, timeout=0.4)          ###//////////////////
+        response = requests.get(url, timeout=0.8)          ###//////////////////
         if response.status_code == 200:
             return url
     except requests.exceptions.RequestException:
@@ -158,7 +158,7 @@ for url in urls:
             url_x = f"{base_url}{ip_address}"
 
             json_url = f"{url}"
-            response = requests.get(json_url, timeout=0.4)                        ####///////////////
+            response = requests.get(json_url, timeout=0.8)                        ####///////////////
             json_data = response.json()
 
             try:
