6c5cc358a3ac34d5d6425c49b0d4e7d3e73c0319
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-23 22:56:05 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -60,6 +60,7 @@ urls = [
     #"https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22henan%22",    #河南
     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5bm%2F6KW%2FIg%3D%3D",    #广西 壮族
     "https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22guangxi%22",  #广西
+    "http://tonkiang.us/hoteliptv.php?s=",
  ]
 
 def modify_urls(url):
