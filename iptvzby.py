da50ca67094feba3801e2735155febc43610e696
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-26 06:42:36 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -12,7 +12,7 @@ from datetime import datetime
 #  获取远程港澳台直播源文件
 url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt"          #源采集地址
 r = requests.get(url)
-open('DIYP-v4.txt'，'wb').write(r.content)         #打开源文件名
+open('DIYP-v4.txt','wb').write(r.content)         #打开源文件名
 
 keywords = ['ysp.php', '苏州-4K', 'CCTV-4K']  # 需要提取的关键字列表
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
@@ -23,11 +23,6 @@ with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('DL.txt', 'w', enc
         if re.search(pattern, line):  # 如果源文件行中有任意关键字
            DL.write(line)  # 将该行写入输出文件
             
-#  获取远程港澳台直播源文件
-url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt"          #源采集地址
-r = requests.get(url)
-open('DIYP-v4.txt'，'wb').write(r.content)         #打开源文件名
-
 keywords = ['AMC电影', '三立戏剧', '天映', '凤凰香港', '星光视界', '八大戏剧']  # 需要提取的关键字列表
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
 #pattern = r"^(.*?),(?!#genre#)(.*?)$"  #以分类直接复制
@@ -37,10 +32,6 @@ with open('DIYP-v4.txt', 'r', encoding='utf-8') as file, open('HK.txt', 'w', enc
         if re.search(pattern, line):  # 如果源文件行中有任意关键字
            HK.write(line)  # 将该行写入输出文件
             
-url = "https://mirror.ghproxy.com/https://raw.githubusercontent.com/Fairy8o/IPTV/main/DIYP-v4.txt"
-r = requests.get(url)
-open('DIYP-v4.txt','wb').write(r.content)
-
 keywords = ['重温经典', '热剧 8M1080', '超级电影 8M1080', '超级电视剧 8M1080', '喜剧 8M1080', '惊悚悬疑 8M1080', '明星大片 8M1080', '潮妈辣婆 8M1080', '精品大剧 8M1080', '动作电影 8M1080', '古装剧场 8M1080', '中国功夫 8M1080', '神乐剧场']  # 需要提取的关键字列表
 pattern = '|'.join(keywords)  # 创建正则表达式模式，匹配任意一个关键字
 #pattern = r"^(.*?),(?!#genre#)(.*?)$" #以分类直接复制
