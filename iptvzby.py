96295658d6f9cb2605c80e40048fde50cf2cceb9
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-29 20:00:43 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -36,6 +36,7 @@ with open("GAT.txt", "w", encoding="utf-8") as output:
 
 
 # 扫源测绘空间地址
+# 搜素关键词："iptv/live/zh_cn.js" && country="CN" && region="Hunan" && city="changsha"
 urls = [
     #"https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0iSGViZWki",  # Hebei (河北)
     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rKz5Y2XIg%3D%3D",  # 河南
