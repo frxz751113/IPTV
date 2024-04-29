e1d4be35fab7e181eb197f9ea407789835cbdf18
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-29 23:53:30 +0800

    Update hn.py

--- hn.py
+++ hn.py
@@ -3,45 +3,48 @@ import concurrent.futures
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
 import requests
+import json
 import re
 import os
 import threading
 from queue import Queue
-from datetime import datetime
 
 
-# 扫源测绘空间地址
+# 网址：https://fofa.info/和https://www.zoomeye.org
 # 搜素关键词："iptv/live/zh_cn.js" && country="CN" && region="Hunan" && city="changsha"
-urls = [
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIGNpdHk9ImNoYW5nc2hhIg%3D%3D",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ibG91ZGki",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iemh1emhvdSI%3D",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieXVleWFuZyI%3D",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iaGVuZ3lhbmci",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iY2hlbnpob3Ui",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieGlhbmd0YW4i",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iY2hhbmdkZSI%3D",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieWl5YW5nIg%3D%3D",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieW9uZ3pob3Ui",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iaHVhaWh1YSI%3D",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ic2hhb3lhbmci",
-     "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iemhhbmdqaWFqaWUi",
-     "https://www.zoomeye.org/searchResult?q=city:%22changsha%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22hengyang%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22zhuzhou%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22yueyang%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22loudi%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22chenzhou%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22xiangtan%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22changde%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22yiyang%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22yongzhou%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22huaihua%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22xiangxi%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22shaoyang%22",
-     "https://www.zoomeye.org/searchResult?q=city%3A%22zhangjiajie%22", 
-    ]
 
+regions = {
+    "湖南": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIHJlZ2lvbj0i5rmW5Y2XIg%3D%3D",
+    "长沙": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY291bnRyeT0iQ04iICYmIGNpdHk9ImNoYW5nc2hhIg%3D%3D",
+    "娄底": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ibG91ZGki",
+    "株洲": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iemh1emhvdSI%3D",
+    "岳阳":" https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieXVleWFuZyI%3D",
+    "衡阳": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iaGVuZ3lhbmci",
+    "郴州": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iY2hlbnpob3Ui",
+    "湘潭": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieGlhbmd0YW4i",
+    "常德": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iY2hhbmdkZSI%3D",
+    "益阳": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieWl5YW5nIg%3D%3D",
+    "永州": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ieW9uZ3pob3Ui",
+    "怀化": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iaHVhaWh1YSI%3D",
+    "邵阳": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0ic2hhb3lhbmci",
+    "张家界": "https://fofa.info/result?qbase64=ImlwdHYvbGl2ZS96aF9jbi5qcyIgJiYgY2l0eT0iemhhbmdqaWFqaWUi",
+
+    "湖南2":"https://www.zoomeye.org/searchResult?q=%2Fiptv%2Flive%2Fzh_cn.js%20%2Bcountry%3A%22CN%22%20%2Bsubdivisions%3A%22hunan%22",
+    "长沙2": "https://www.zoomeye.org/searchResult?q=city:%22changsha%22",
+    "衡阳2": "https://www.zoomeye.org/searchResult?q=city%3A%22hengyang%22",
+    "株洲2": "https://www.zoomeye.org/searchResult?q=city%3A%22zhuzhou%22",
+    "岳阳2": "https://www.zoomeye.org/searchResult?q=city%3A%22yueyang%22",
+    "娄底2": "https://www.zoomeye.org/searchResult?q=city%3A%22loudi%22",
+    "郴州2": "https://www.zoomeye.org/searchResult?q=city%3A%22chenzhou%22",
+    "湘潭2": "https://www.zoomeye.org/searchResult?q=city%3A%22xiangtan%22",
+    "常德2": "https://www.zoomeye.org/searchResult?q=city%3A%22changde%22",
+    "益阳2": "https://www.zoomeye.org/searchResult?q=city%3A%22yiyang%22",
+    "永州2": "https://www.zoomeye.org/searchResult?q=city%3A%22yongzhou%22",
+    "怀化2": "https://www.zoomeye.org/searchResult?q=city%3A%22huaihua%22",
+    "湘西2": "https://www.zoomeye.org/searchResult?q=city%3A%22xiangxi%22",
+    "邵阳2": "https://www.zoomeye.org/searchResult?q=city%3A%22shaoyang%22",
+    "张家界2": "https://www.zoomeye.org/searchResult?q=city%3A%22zhangjiajie%22",
+}
 def modify_urls(url):
     modified_urls = []
     ip_start_index = url.find("//") + 2
@@ -57,20 +60,18 @@ def modify_urls(url):
 
     return modified_urls
 
-
 def is_url_accessible(url):
     try:
-        response = requests.get(url, timeout=1)          ###//////////////////
+        response = requests.get(url, timeout=0.5)
         if response.status_code == 200:
             return url
     except requests.exceptions.RequestException:
         pass
+
     return None
 
 
-results = []
-
-for url in urls:
+def process_url(region, url):
     # 创建一个Chrome WebDriver实例
     chrome_options = Options()
     chrome_options.add_argument('--headless')
@@ -90,10 +91,9 @@ for url in urls:
     # 查找所有符合指定格式的网址
     pattern = r"http://\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}:\d+"  # 设置匹配的格式，如http://8.8.8.8:8888
     urls_all = re.findall(pattern, page_content)
-    # urls = list(set(urls_all))  # 去重得到唯一的URL列表
     urls = set(urls_all)  # 去重得到唯一的URL列表
     x_urls = []
-    for url in urls:  # 对urls进行处理，ip第四位修改为1，并去重
+    for url in urls: # 对urls进行处理，ip第四位修改为1，并去重
         url = url.strip()
         ip_start_index = url.find("//") + 2
         ip_end_index = url.find(":", ip_start_index)
@@ -107,11 +107,11 @@ for url in urls:
         modified_ip = f"{ip_address}{ip_end}"
         x_url = f"{base_url}{modified_ip}{port}"
         x_urls.append(x_url)
-    urls = set(x_urls)  # 去重得到唯一的URL列表
+    urls = set(x_urls) # 去重得到唯一的URL列表
 
     valid_urls = []
     #   多线程获取可用url
-    with concurrent.futures.ThreadPoolExecutor(max_workers=100) as executor:
+    with concurrent.futures.ThreadPoolExecutor(max_workers = 100) as executor:
         futures = []
         for url in urls:
             url = url.strip()
@@ -127,6 +127,7 @@ for url in urls:
     for url in valid_urls:
         print(url)
     # 遍历网址列表，获取JSON文件并解析
+    results = []
     for url in valid_urls:
         try:
             # 发送GET请求获取JSON文件，设置超时时间为0.5秒
@@ -138,7 +139,7 @@ for url in urls:
             url_x = f"{base_url}{ip_address}"
 
             json_url = f"{url}"
-            response = requests.get(json_url, timeout=1)                        ####///////////////
+            response = requests.get(json_url, timeout=0.5)
             json_data = response.json()
 
             try:
@@ -148,16 +149,16 @@ for url in urls:
                         name = item.get('name')
                         urlx = item.get('url')
                         if ',' in urlx:
-                            urlx = f"aaaaaaaa"
-
-                        if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
-                        #if 'http' in urlx:
+                            urlx=f"aaaaaaaa"
+                            
+                        #if 'http' in urlx or 'udp' in urlx or 'rtp' in urlx:
+                        if 'http' in urlx:
                             urld = f"{urlx}"
                         else:
                             urld = f"{url_x}{urlx}"
 
-                        if name and urld:
-                            # 替换特定文字
+                        if name and urlx:
+                            # 删除特定文字
                             name = name.replace("中央", "CCTV")
                             name = name.replace("高清", "")
                             name = name.replace("HD", "")
@@ -165,12 +166,6 @@ for url in urls:
                             name = name.replace("超高", "")
                             name = name.replace("频道", "")
                             name = name.replace("-", "")
-                            name = name.replace("002", "酒店MV")
-                            name = name.replace("305", "酒店影视1")
-                            name = name.replace("306", "酒店影视2")
-                            name = name.replace("307", "酒店影视3")
-                            name = name.replace("莲花台", "凤凰香港")
-                            name = name.replace("007广西影视一3", "广西影视")
                             name = name.replace(" ", "")
                             name = name.replace("PLUS", "+")
                             name = name.replace("＋", "+")
@@ -226,6 +221,7 @@ for url in urls:
                             name = name.replace("内蒙卫视", "内蒙古卫视")
                             name = name.replace("福建东南卫视", "东南卫视")
                             name = name.replace("广东南方卫视", "南方卫视")
+                            name = name.replace("金鹰卡通卫视", "金鹰卡通")
                             name = name.replace("湖南金鹰卡通", "金鹰卡通")
                             name = name.replace("炫动卡通", "哈哈炫动")
                             name = name.replace("卡酷卡通", "卡酷少儿")
@@ -242,29 +238,74 @@ for url in urls:
                             name = name.replace("卫视台", "卫视")
                             name = name.replace("湖南电视台", "湖南卫视")
                             name = name.replace("少儿科教", "少儿")
-                            name = name.replace("TV星河2）", "星河")
                             name = name.replace("影视剧", "影视")
-                            name = name.replace("电视剧", "影视")
-                            name = name.replace("卡", "")
                             results.append(f"{name},{urld}")
+                            
             except:
                 continue
         except:
             continue
 
-channels = []
+    return results
 
-for result in results:
-    line = result.strip()
-    if result:
-        channel_name, channel_url = result.split(',')
-        channels.append((channel_name, channel_url))
+# 将结果保存到文本文件
+def save_results(results, filename):
+    with open(filename, "w", encoding="utf-8") as file:
+        for result in results:
+            file.write(result + "\n")
+            print(result)
 
-with open("iptv.txt", 'w', encoding='utf-8') as file:
-    for result in results:
-        file.write(result + "\n")
-        print(result)
-print("频道列表文件iptv.txt获取完成！")
+directory = "地区源"
+# 文件目录不存在，则创建文件目录        
+if not os.path.exists(directory):
+    os.makedirs(directory)    
+    # 删除旧文件
+existing_files = os.listdir(directory)
+for file_name in existing_files:
+    file_path = os.path.join(directory, file_name)
+    if os.path.isfile(file_path):
+        os.remove(file_path)
+
+for region, url in regions.items():
+    results = process_url(region, url)
+    save_results(results, os.path.join(directory, f"{region}.txt"))
+
+# 合并结果成一个文件
+def merge_txt_files():
+    # 获取当前脚本所在目录
+    script_dir = os.path.dirname(os.path.abspath(__file__))
+    dir_path = os.path.join(script_dir, "地区源")
+
+    # 检查目录是否存在
+    if not os.path.exists(dir_path):
+        print(f"目录 '地区源' 不存在.")
+        return
+
+    # 获取目录下所有的txt文件
+    txt_files = [file for file in os.listdir(dir_path) if file.endswith(".txt")]
+
+    # 检查是否有txt文件
+    if not txt_files:
+        print(f"目录 '地区源' 中没有找到txt文件.")
+        return
+
+    # 合并文件内容
+    merged_content = ""
+    for txt_file in txt_files:
+        file_path = os.path.join(dir_path, txt_file)
+        with open(file_path, "r", encoding="utf-8") as file:
+            merged_content += file.read() + "\n"
+
+    # 将合并后的内容写入新文件
+    merged_file_path = os.path.join(script_dir, "JDY.txt")
+    with open(merged_file_path, "w", encoding="utf-8") as merged_file:
+        merged_file.write(merged_content)
+
+# 调用函数
+merge_txt_files()
+
+print("频道获取完成，频道列表文件保存为JDY.txt！")
+
 
 import eventlet
 
@@ -447,6 +488,6 @@ with open("湖南.txt", "w", encoding="utf-8") as output:
     #output.write(f"{now.strftime("%Y-%m-%d")},url\n")
     #output.write(f"{now.strftime("%H:%M:%S")},url\n")
 
-os.remove("iptv.txt")
+os.remove("JDY.txt")
 os.remove("hn.txt")
 print("任务运行完毕，分类频道列表可查看文件夹内湖南.txt文件！")
