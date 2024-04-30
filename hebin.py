d32d388d8f4f3d89f06518040a0a696d803da202
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-04-30 23:04:15 +0800

    Update hebin.py

--- hebin.py
+++ hebin.py
@@ -11,14 +11,14 @@ from datetime import datetime
       
 # 合并自定义频道文件内容
 file_contents = []
-file_paths = ["gatai.txt","iptv_list.txt",]  # 替换为实际的文件路径列表
+file_paths = ["gatai.txt","iptv.txt",]  # 替换为实际的文件路径列表
 for file_path in file_paths:
     with open(file_path, 'r', encoding="utf-8") as file:
         content = file.read()
         file_contents.append(content)
 
 # 写入合并后的文件
-with open("iptv.txt",  "w", encoding="utf-8") as output:
+with open("iptv_list.txt",  "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
 
-print("任务运行完毕，分类频道列表可查看文件夹内iptv.txt文件！")
+print("任务运行完毕，分类频道列表可查看文件夹内iptv_list.txt文件！")
