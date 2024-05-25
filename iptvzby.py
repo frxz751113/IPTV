19ce9090f068dfa3d8b26a9f6253bc6e97e5c99a
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-25 15:33:12 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -340,7 +340,7 @@ def worker():
             ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
 
             # 获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(1, False):  #################////////////////////////////////
+            with eventlet.Timeout(0.8, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
@@ -375,7 +375,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 16
+num_threads = 8
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
@@ -401,7 +401,7 @@ def channel_key(channel_name):
 # 对频道进行排序
 results.sort(key=lambda x: (x[0], -float(x[2].split()[0])))
 results.sort(key=lambda x: channel_key(x[0]))
-result_counter = 10  # 每个频道需要的个数
+result_counter = 3  # 每个频道需要的个数
 
 with open("hn.txt", 'w', encoding='utf-8') as file:
     channel_counters = {}
