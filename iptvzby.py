7ff86978ec872b3dc594c2cb86bda757e65a7186
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-25 11:02:10 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -344,7 +344,7 @@ def worker():
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
-                response_time = (end_time - start_time) * 0.1
+                response_time = (end_time - start_time) * 2
 
             if content:
                 with open(ts_lists_0, 'ab') as f:
