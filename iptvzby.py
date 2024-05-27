82e3d169faf7374157a3818e537b00d258f20ab4
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-05-27 19:37:02 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -480,11 +480,37 @@ for file_path in file_paths:
 with open("iptv_list.txt", "w", encoding="utf-8") as output:
     output.write('\n'.join(file_contents))
 
-# 写入更新日期时间
-    #now = datetime.now()
-    #output.write(f"更新时间,#genre#\n")
-    #output.write(f"{now.strftime("%Y-%m-%d")},url\n")
-    #output.write(f"{now.strftime("%H:%M:%S")},url\n")
+def txt_to_m3u(input_file, output_file):
+    # 读取txt文件内容
+    with open(input_file, 'r', encoding='utf-8') as f:
+        lines = f.readlines()
+
+    # 打开m3u文件并写入内容
+    with open(output_file, 'w', encoding='utf-8') as f:
+        f.write('#EXTM3U x-tvg-url="https://live.fanmingming.com/e.xml" catchup="append" catchup-source="?playseek=${(b)yyyyMMddHHmmss}-${(e)yyyyMMddHHmmss}"\n')
+
+        # 初始化genre变量
+        genre = ''
+
+        # 遍历txt文件内容
+        for line in lines:
+            line = line.strip()
+            if "," in line:  # 防止文件里面缺失“,”号报错
+                # if line:
+                # 检查是否是genre行
+                channel_name, channel_url = line.split(',', 1)
+                if channel_url == '#genre#':
+                    genre = channel_name
+                    print(genre)
+                else:
+                    # 将频道信息写入m3u文件
+                    f.write(f'#EXTINF:-1 tvg-logo="https://raw.githubusercontent.com/linitfor/epg/main/logo/{channel_name}.png" group-title="{genre}",{channel_name}\n')
+                    f.write(f'{channel_url}\n')
+
+
+# 将txt文件转换为m3u文件
+txt_to_m3u('iptv_list.txt', 'iptv_list.m3u')
+printprint(f"成功寫出M3U file")
 
 os.remove("iptv.txt")
 os.remove("GAT.txt")
