5b90adaac52726bd8e1380be3bc3597c422e8fbc
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-08 12:54:21 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -168,7 +168,9 @@ for url in urls:
                         if ',' in urlx:
                             urlx = f"aaaaaaaa"
 
-                        if 'http' in urlx if 'udp' in urlx or 'rtp' in urlx:######################
+                        
+                        if 'udp' in line['url'] or 'rtp' in line['url']:
+                            program_judgment('{},{}'.format(line['name'],'http://{}/tsfile/live/{}_{}.m3u8'.format(info,line['chid'],line['srcid'])))
                         #if 'http' in urlx or 'rtsp' in urlx:
                             urld = f"{urlx}"
                         else:
