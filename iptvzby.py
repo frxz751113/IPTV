3e618426fdef8cfadd123e34118dfd047889a2a5
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-11 20:52:33 +0800

    Update iptvzby.py

--- iptvzby.py
+++ iptvzby.py
@@ -356,7 +356,7 @@ def worker():
             
 
             # 获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(64, False):  #################////////////////////////////////
+            with eventlet.Timeout(264, False):  #################////////////////////////////////
                 start_time = time.time()
                 content = requests.get(ts_url).content
                 end_time = time.time()
@@ -391,7 +391,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 64
+num_threads = 156
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True)
     # t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
