c8ecd5e82ae34e31c22beb4ca1b522ac8ac07494
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-13 22:40:02 +0800

    Update 自用.py

--- 自用.py
+++ 自用.py
@@ -8,7 +8,7 @@ with open('iptv_list.txt', 'r', encoding='utf-8') as f:  #打开文件，并对�
  with open('iptv_list.txt', 'r', encoding='utf-8') as file, open('a.txt', 'w', encoding='utf-8') as a:           ###########
     a.write('\n央视,#genre#\n')                                                                        ###########
     for line in file:
-     if 'wh7f' not in line and 'ottrrs' not in line:
+     if 'wh7f' not in line and 'ottrrs' not in line and '失效' not in line and '更新' not in line:
         if re.search(pattern, line):  # 如果行中有任意关键字                                                ###########
           a.write(line)  # 将该行写入输出文件                                                               ###########
                                                                                                            ###########
@@ -21,7 +21,7 @@ with open('iptv_list.txt', 'r', encoding='utf-8') as f:  #打开文件，并对�
  with open('iptv_list.txt', 'r', encoding='utf-8') as file, open('b.txt', 'w', encoding='utf-8') as b:           ###########
     b.write('\n卫视,#genre#\n')                                                                        ###########
     for line in file:
-      if 'wh7f' not in line and 'ottrrs' not in line:
+      if 'wh7f' not in line and 'ottrrs' not in line and '失效' not in line and '更新' not in line:
         if re.search(pattern, line):  # 如果行中有任意关键字                                                ###########
           b.write(line)  # 将该行写入输出文件                                                               ###########
                                                   
@@ -37,7 +37,7 @@ with open('iptv_list.txt', 'r', encoding='utf-8') as f:  #打开文件，并对�
  with open('iptv_list.txt', 'r', encoding='utf-8') as file, open('c.txt', 'w', encoding='utf-8') as c:           ###########
     c.write('\n省市,#genre#\n')                                                                        ###########
     for line in file:
-      if 'wh7f' not in line and 'ottrrs' not in line:
+      if 'wh7f' not in line and 'ottrrs' not in line and '失效' not in line and '更新' not in line:
         if re.search(pattern, line):  # 如果行中有任意关键字 
           c.write(line)  # 将该行写入输出文件
                                                   
@@ -49,7 +49,7 @@ with open('iptv_list.txt', 'r', encoding='utf-8') as f:  #打开文件，并对�
  with open('iptv_list.txt', 'r', encoding='utf-8') as file, open('d.txt', 'w', encoding='utf-8') as d:           ###########
     d.write('\n港澳,#genre#\n')                                                                        ###########
     for line in file:
-      if 'wh7f' not in line and 'ottrrs' not in line:
+      if 'wh7f' not in line and 'ottrrs' not in line and '失效' not in line and '更新' not in line:
         if re.search(pattern, line):  # 如果行中有任意关键字                                                ###########
           d.write(line)  # 将该行写入输出文件                                                               ###########
                                                   
