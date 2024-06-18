e7327f88e4aaa2e6400738525888e84bfae2472a
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-18 13:13:43 +0800

    Update qita.py

--- qita.py
+++ qita.py
@@ -42,7 +42,7 @@ def worker():
             ts_url = channel_url_t + ts_lists[0]  # 拼接单个视频片段下载链接
 
             # 多获取的视频数据进行5秒钟限制
-            with eventlet.Timeout(100, False):####################################################################################
+            with eventlet.Timeout(20, False):####################################################################################
                 start_time = time.time()
                 content = requests.get(ts_url,timeout=1).content
                 end_time = time.time()
@@ -75,7 +75,7 @@ def worker():
 
 
 # 创建多个工作线程
-num_threads = 20
+num_threads = 4
 for _ in range(num_threads):
     t = threading.Thread(target=worker, daemon=True) 
     #t = threading.Thread(target=worker, args=(event,len(channels)))  # 将工作线程设置为守护线程
