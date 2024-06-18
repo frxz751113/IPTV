05f0f6ac10044234c6d67e55439314e33565eca4
frxz751113 <156018267+frxz751113@users.noreply.github.com>
2024-06-18 12:26:05 +0800

    Update qita.py

--- qita.py
+++ qita.py
@@ -17,7 +17,7 @@ results = []
 channels = []
 error_channels = []
 
-with open("tv/itv.txt", 'r', encoding='utf-8') as file:
+with open("iptv_list.txt", 'r', encoding='utf-8') as file:
     lines = file.readlines()
     for line in lines:
         line = line.strip()
